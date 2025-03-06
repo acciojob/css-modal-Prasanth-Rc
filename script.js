f2894b5234f5b0bb37d34a4e743170c49c5c6c9e
Prasanth R <97664738+Prasanth-Rc@users.noreply.github.com>
2025-03-06 19:36:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const modal = document.getElementById('modal');
+const openModalButton = document.getElementById('openModal');
+const closeModalButton = document.querySelector('.close-modal');
+
+openModalButton.addEventListener('click', function () {
+  modal.style.display = 'block';
+});
+
+closeModalButton.addEventListener('click', function () {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', function (event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
